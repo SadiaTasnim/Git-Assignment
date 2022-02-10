42664ac2788cee6dbd22c7cc5503c479e1b52f84
shahriar1212 <chowdhury.shahriar12@gmail.com>
2022-02-10 23:10:42 +0600

    button to change color

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-var a = 5
-var b = 6
-c = a + b
+var color = ["#222f3e","#f368ae", "#03045E", "#00B4D8", "#C0A080", , "#ED5EDD", "#EFEFEF", , "#2666CF", "#F5F2E7", , "#F76E11", "#8A39E1", , "#EDCDBB", "#FA4EAB"];
+
+var i = 0;
+document.querySelector("button").addEventListener("click",
+function(){
+    i = i < color.length ? ++i : 0;
+    document.querySelector("body").style.background = color[i]
+})
