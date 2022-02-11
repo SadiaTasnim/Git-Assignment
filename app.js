39eceadd2b803bd98f3ddc2c283ab1525cf3e496
shahriar1212 <chowdhury.shahriar12@gmail.com>
2022-02-11 11:55:20 +0600

    changing color

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var a = 5
-var b = 6
-var c = a + b
 
-var d = a*b
+var color = ["#222f3e","#f368ae", "#03045E", "#00B4D8", "#C0A080", , "#ED5EDD", "#EFEFEF", , "#2666CF", "#F5F2E7", , "#F76E11", "#8A39E1", , "#EDCDBB", "#FA4EAB"];
+
+var i = 0;
+document.querySelector("button").addEventListener("click",
+function(){
+    i = i < color.length ? ++i : 0;
+    document.querySelector("body").style.background = color[i]
+})
